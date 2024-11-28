18489bda78d2e15ae42147152cbe448a1673f944
trung12345450 <trungdaica22062004@gmail.com>
2024-11-28 22:36:49 +0700

    bài tập

---
+++ BTVN/header.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Administration </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Trang chủ <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Trang ngoài</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Thể loại </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link"  href="#">Tác giả </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Bài viết </a>
+        </li>
+        </ul>
+    </div>
+</nav>
